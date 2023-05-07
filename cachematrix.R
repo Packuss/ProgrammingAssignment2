6a9bf625710d5cd5f2e9c619fc346f9e8b54d0e0
Packuss <78770451+Packuss@users.noreply.github.com>
2023-05-06 21:34:02 -0500

    Update cachematrix.R
    Added code for two R functions: makeCacheMatrix and cacheSolve, which allow for matrix inversion and caching of the inverse. These functions were developed as part of a programming assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## A function that creates a matrix object and its cache
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        # Initialize inverse as NULL
+        inverse <- NULL
+        
+        # Function to set the matrix
+        set <- function(y) {
+                
+                # Set the matrix
+                x <<- y
+                # Set the inverse as NULL to avoid errors
+                inverse <<- NULL
+        }
+        
+        # Function to get the matrix
+        get <- function() x
+        
+        # Function to set the cached inverse
+        setInverse <- function(inv) { 
+                inverse <<- inv
+        }
+        
+        # Function to get the cached inverse
+        getInverse <- function() {
+                inverse
+        }
+        
+        # Return list of functions
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## A function that computes and caches the inverse of a matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse()
+        if(!is.null(inv)) {
+                message("Getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setInverse(inv)
+        inv
 }
+
+## Test the functions
+mymatrix <- makeCacheMatrix()
+mymatrix$set(matrix(c(1.123,2.123,3.123,4.234,5.456,6.234,7.234,8.234,9.432), nrow = 3))
+cacheSolve(mymatrix)
